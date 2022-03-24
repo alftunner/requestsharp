c5183be330a1238c5c4b1c8440ad4bf7041d2187
alftunner <alftunner@gmail.com>
2022-03-24 20:50:23 +0300

    this is my second commit

--- forGitLearning/Program.cs
+++ forGitLearning/Program.cs
@@ -1,4 +1,27 @@
-﻿using forGitLearning;
+﻿/*using forGitLearning;
 
 Request request = new Request();
-request.PrintInfoRequest();
+request.PrintInfoRequest();*/
+
+void Change(out int i, ref int[] arr)
+{
+    i = 20;
+    Console.WriteLine("Значения до переопределения:");
+    Console.WriteLine("i = " + i + " arr[0] = " + arr[0]);
+
+    
+    arr[0] = 20;
+    
+    Console.WriteLine("Значения после переопределения:");
+    Console.WriteLine("i = " + i + " arr[0] = " + arr[0]);
+}
+
+int i ;
+int[] arr = {10, 20, 30};
+Console.WriteLine("Начальное состояние:");
+Console.WriteLine("i = " + i + " arr[0] = " + arr[0]);
+
+Change(out i, ref arr);
+
+Console.WriteLine("FINAL:");
+Console.WriteLine("i = " + i + " arr[0] = " + arr[0]);
